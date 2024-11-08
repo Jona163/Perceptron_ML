2610b67b699137f42e49a7c8776cce1e84ef7a69
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-08 15:09:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 # Fecha: 08 Noviembre 2024
 # Descripción: Código para una simulación de Perceptrón.
 # GitHub: https://github.com/Jona163
+#Importancion de liberias 
+import numpy as np
+
+# Función escalón unitario (unit step function)
+# Retorna 1 si x > 0, de lo contrario retorna 0.
+def unit_step_func(x):
+    return np.where(x > 0 , 1, 0)
