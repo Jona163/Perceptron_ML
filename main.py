71dca261b8c352341f5f5fc15a0f1685bfad5200
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-08 15:09:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ import numpy as np
 # Retorna 1 si x > 0, de lo contrario retorna 0.
 def unit_step_func(x):
     return np.where(x > 0 , 1, 0)
+
+# Clase Perceptron
+class Perceptron:
+
+    # Inicialización del perceptrón con tasa de aprendizaje y número de iteraciones
+    def __init__(self, learning_rate=0.01, n_iters=1000):
+        self.lr = learning_rate  # Tasa de aprendizaje
+        self.n_iters = n_iters   # Número de iteraciones
+        self.activation_func = unit_step_func  # Función de activación (escalón unitario)
+        self.weights = None      # Pesos (inicialmente indefinidos)
+        self.bias = None         # Sesgo (inicialmente indefinido)
